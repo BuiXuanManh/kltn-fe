0c7deb5405e152f3628fcc9a7fb1dc8be4dea3d9
BuiXuanManh <91316469+BuiXuanManh@users.noreply.github.com>
2024-03-20 21:05:12 +0700

    upload ui sach moi

--- src/component/pages/home/Home.jsx
+++ src/component/pages/home/Home.jsx
@@ -54,6 +54,7 @@ const Home = () => {
         genre: 'Tâm lý - Tự viện',
         description: 'Một quyển sách kinh điển về tâm lý học.'
     }];
+    var rates = [{ id: 1, rate: 5, name: "nam1" }, { id: 2, rate: 4, name: "nam1" }, { id: 3, rate: 3, name: "nam1" }, { id: 4, rate: 2, name: "nam1" }, { id: 5, rate: 1, name: "nam1" }]
 
     return (< div className='bg-gray-100'>
         <Carousel interval={3000} >
@@ -68,7 +69,7 @@ const Home = () => {
             })
             }
         </Carousel>
-        <div className='grid grid-cols-3 mt-10 mx-48 border border-white rounded-lg bg-white'>
+        <div className='grid grid-cols-3 mt-10 mx-48 border border-white rounded-lg bg-white shadow-md'>
             <div className='px-4 grid grid-cols-1 col-span-2 w-full'>
                 <div className='flex justify-between'>
                     <h3 className='p-3 items-start text-start'>Sach de cu</h3>
@@ -76,17 +77,17 @@ const Home = () => {
                 </div>
             </div>
             <div className='px- 4 grid grid-cols-1 w-full'>
-            <div className='flex justify-between'>
+                <div className='flex justify-between'>
                     <h3 className='p-3 items-start text-start'>Dang doc</h3>
                     <a href='/books' className='p-3 items-start text-start text-orange-500 text-sm font-serif'>Xem tat ca</a>
                 </div>
             </div>
         </div>
-        <div className='grid grid-cols-3 mx-48 border border-white rounded-lg bg-white'>
+        <div className='grid grid-cols-3 mx-48 border border-white rounded-lg bg-white  shadow-md'>
             <div className='px-4 grid grid-cols-2 col-span-2 gap-4 max-w-full justify-start'>
                 {data.map((item) => {
                     return (
-                        <div key={item.id} className='flex text-start w-full mt-5 max-h-52'>
+                        <div key={item.id} className='flex text-start w-full mt-5 max-h-52  shadow-md'>
                             <div className='w-24 h-32'>
                                 <img className='w-full h-full object-cover' src={item.img} alt='img book' />
                             </div>
@@ -102,10 +103,10 @@ const Home = () => {
                     );
                 })}
             </div>
-            <div className='px-4 grid grid-cols-1 gap-4 max-w-full items-start'>
+            <div className='px-4 pb-4 grid grid-cols-1 gap-4 max-w-full items-start shadow-md'>
                 {data.map((item) => {
                     return (
-                        <div key={item.id} className='flex text-start w-full mt-5 max-h-30'>
+                        <div key={item.id} className='flex text-start w-full mt-5 max-h-30 pb-2 shadow-md '>
                             <div className='w-8 h-12'>
                                 <img width={40} height={60} className='w-full h-full' src={item.img} alt='img book' />
                             </div>
@@ -117,6 +118,62 @@ const Home = () => {
                     );
                 })}
             </div>
+
+        </div>
+        {/* New update book */}
+        <div className='grid grid-cols-3 mt-10 mx-48 border border-white rounded-lg bg-white'>
+            <div className='px-4 grid grid-cols-1 col-span-2 w-full'>
+                <div className='flex justify-between'>
+                    <h3 className='p-3 items-start text-start'>Sach moi</h3>
+                    <a href='/books' className='p-3 items-start text-start text-orange-500 text-sm font-serif'>Xem tat ca</a>
+                </div>
+            </div>
+            <div className='px- 4 grid grid-cols-1 w-full'>
+                <div className='flex justify-between'>
+                    <h3 className='p-3 items-start text-start'>Moi danh gia</h3>
+                    <a href='/books' className='p-3 items-start text-start text-orange-500 text-sm font-serif'>Xem tat ca</a>
+                </div>
+            </div>
+        </div>
+        <div className='grid grid-cols-3 mx-48 border border-white rounded-lg bg-white  shadow-md'>
+            <div className='px-4 col-span-2 gap-4 max-w-full justify-start'>
+                {data.map((item) => {
+                    return (
+                        <div key={item.id} className='flex text-start w-full mt-5 max-h-52  shadow-md'>
+                            <div className='w-24 h-32'>
+                                <img className='w-full h-full object-cover' src={item.img} alt='img book' />
+                            </div>
+                            <div className='ml-2 w-full'>
+                                <h3>{item.title}</h3>
+
+                                <div className='flex items-center justify-between w-full'>
+                                    <div className=''>{item.author}</div>
+                                    <div className='p-1 border border-yellow-400 text-orange-600'>{item.genre}</div>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+            <div className='px-4 grid grid-cols-1 gap-4 max-w-full shadow-md pb-4'>
+                {rates.map((item) => {
+                    return (
+                        <div key={item.id} className='flex items-center justify-between w-full mt-5 pb-2 shadow-md'>
+                            <div className='flex'>
+                                <div className='w-8 h-12'>
+                                    <img width={40} height={60} className='w-full h-full' src={item.img} alt='img book' />
+                                </div>
+                                <div>
+                                    <h3>{item.name}</h3>
+                                    <h3>{item.id}</h3>
+                                </div>
+                            </div>
+                            <div className='text-center'><span>{item.rate}</span></div>
+                        </div>
+                    );
+                })}
+            </div>
+
 
         </div>
 
