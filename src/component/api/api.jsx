9964baf22ad81a9902965b3d385df95bb9ddbd26
BuiXuanManh <91316469+BuiXuanManh@users.noreply.github.com>
2024-05-31 00:17:44 +0700

    deploy vercel

--- src/component/api/api.jsx
+++ src/component/api/api.jsx
@@ -9,8 +9,7 @@ class AxiosSingleton {
         }
 
         this.api = axios.create({
-            // baseURL: 'http://ec2-13-212-191-226.ap-southeast-1.compute.amazonaws.com:8080',
-            baseURL: 'http://ec2-52-221-196-43.ap-southeast-1.compute.amazonaws.com:8080',
+            baseURL: 'https://ec2-52-221-196-43.ap-southeast-1.compute.amazonaws.com:8080',
             headers: {
                 'Content-Type': 'application/json',
             },
